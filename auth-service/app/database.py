a05cd232ec08c47131bb51e81b2181d727be7521
Sanjeet Kumar <20pgmca16@gmail.com>
2025-06-07 12:26:27 +0530

    Trigger CI/CD pipeline2

--- auth-service/app/database.py
+++ auth-service/app/database.py
@@ -1,9 +1,19 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./auth.db"
+load_dotenv(dotenv_path="/app/.env")  # only if running locally with .env
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
